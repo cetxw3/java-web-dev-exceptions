9445836f7d39567ae8ec3a32b2c8e90bd38219cd
Cera Thomason <cetxw3@gmail.com>
2022-05-10 18:34:02 -0500

    finished exercises

---
+++ src/org/launchcode/exercises/FileExtensionException.java
@@ -0,0 +1,10 @@
+package org.launchcode.exercises;
+
+public class FileExtensionException extends Exception {
+
+    public FileExtensionException(String message) {
+        super(message);
+    }
+
+
+}
--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -9,23 +9,52 @@ public class Main {
 
         // Test out your Divide() function!
 
+        System.out.println(Divide(10,2));
+        //System.out.println(Divide(10,0));
+
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
         studentFiles.put("Brad", "");
         studentFiles.put("Elizabeth", "MyCode.java");
-        studentFiles.put("Stefanie", "CoolProgram.java");
+        studentFiles.put("Stefanie", "CoolProgram.jav");
 
         // Test out your CheckFileExtension() function!
+        for (Map.Entry<String, String> x : studentFiles.entrySet()) {
+            System.out.println(x.getKey() + " receives " + CheckFileExtension(x.getValue()) + " points for file extention.");
+        }
+}
+
+    public static double Divide(int x, int y) {
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("Cannot divide by zero!");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
+        return x / y;
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
-    }
+    public static int CheckFileExtension(String fileName) {
+        String lastFive;
+        if (fileName.equals(null) || fileName.equals("")) {
+            lastFive = fileName;
+        } else {
+            lastFive = fileName.substring(fileName.length()-5);
+        }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+        if (lastFive.equals(".java")) {
+            return 1;
+        } else if (fileName.equals(null) || fileName.equals("")) {
+            try {
+                throw new FileExtensionException("File name is blank or null");
+            } catch (FileExtensionException e) {
+                e.printStackTrace();
+            }
+            return -1;
+        } else {
+            return 0;
+        }
     }
 
 }
